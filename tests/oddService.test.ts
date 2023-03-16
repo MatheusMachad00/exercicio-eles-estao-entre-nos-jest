b507e901c40b0e7eea2abff379de785f1edcccdf
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-03-16 10:27:35 -0300

    Tests finished

--- tests/oddService.test.ts
+++ tests/oddService.test.ts
@@ -1,19 +1,28 @@
 import { oddService } from "../src/services/oddService";
 import { jest } from "@jest/globals";
+import { secretService } from "../src/_secret_/secretService";
+import { areaFiftyOneRepository } from "../src/repositories/areaFiftyOneRepository";
+import { listOVNIS } from "../src/controllers/oddController";
 
 describe("isReallyOdd unit test suite", () => {
   it("Should 'Im not sure...'", () => {
     const number = Math.floor(Math.random() * 100);
+    jest.spyOn(secretService, "isOdd").mockReturnValue(false);
     const service = oddService.isReallyOdd(number);
     expect(service).toBe("Im not sure...");
   });
 
   it("Should 'maybe not, who knows?'", () => {
-    jest.mock("../src/_secret_/secretService", () => {
-      return true;
-    });
     const number = Math.floor(Math.random() * 100);
+    jest.spyOn(secretService, "isOdd").mockReturnValue(true);
     const service = oddService.isReallyOdd(number);
     expect(service).toBe("maybe not, who knows?");
   });
+
+  it("Should return an OVNI array", async () => {
+    jest.spyOn(areaFiftyOneRepository, "listOVNIS").mockResolvedValueOnce([]);
+
+    const result = await oddService.listOVNIS();
+    expect(result).toEqual([]);
+  })
 });
