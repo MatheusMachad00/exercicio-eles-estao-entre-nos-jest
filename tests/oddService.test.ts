8c0a1b3fe2bec273ab7ed1abc112e836bd786fda
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-03-16 10:10:49 -0300

    .env removed

--- tests/oddService.test.ts
+++ tests/oddService.test.ts
@@ -1,5 +1,19 @@
 import { oddService } from "../src/services/oddService";
+import { jest } from "@jest/globals";
 
-describe ("isReallyOdd unit test suite", () => {
-  it()
-})
+describe("isReallyOdd unit test suite", () => {
+  it("Should 'Im not sure...'", () => {
+    const number = Math.floor(Math.random() * 100);
+    const service = oddService.isReallyOdd(number);
+    expect(service).toBe("Im not sure...");
+  });
+
+  it("Should 'maybe not, who knows?'", () => {
+    jest.mock("../src/_secret_/secretService", () => {
+      return true;
+    });
+    const number = Math.floor(Math.random() * 100);
+    const service = oddService.isReallyOdd(number);
+    expect(service).toBe("maybe not, who knows?");
+  });
+});
